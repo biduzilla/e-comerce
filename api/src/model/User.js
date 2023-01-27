0b86646c79955012a4074d10846911a4379a50aa
biduzilla <rlyowlly19@gmail.com>
2023-01-27 11:52:35 -0300

    lista de produtos

--- api/src/model/User.js
+++ api/src/model/User.js
@@ -5,12 +5,20 @@ const userSchema = new mongoose.Schema({
     nome: { type: String, require: true },
     telefone: { type: Number, require: true },
     cidade: { type: String, require: true },
+    produtos: [
+        {
+            nome: { type: String, required: true },
+            descricao: { type: String, required: true },
+            imagem: { type: String, required: true },
+            preco: { type: String, required: true },
+        }
+    ]
 },
     {
         versionKer: false,
     }
 )
 
-const users = mongoose.model('users',userSchema);
+const users = mongoose.model('users', userSchema);
 
 export default users;
